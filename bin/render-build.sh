f8527e1bef49451e110fd27e0a3baa08aa89eae6
masato420 <yamamoto.3583@outlook.jp>
2024-02-29 01:01:00 +0900

    ユーザーモデル作成

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,6 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
+bundle exec rake db:migrate || bundle exec rake db:setup
+bundle exec rails generate devise User
 bundle exec rake db:migrate
