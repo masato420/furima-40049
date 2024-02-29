a34f18622976fcea3255fb8399e04fc23919f607
masato420 <yamamoto.3583@outlook.jp>
2024-02-29 20:49:40 +0900

    user controllar作成

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,4 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate || bundle exec rake db:setup
-bundle exec rails generate devise User
 bundle exec rake db:migrate
