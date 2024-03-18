e88c9bf0d4a57fbb51e57cc29327192a9624b883
masato420 <yamamoto.3583@outlook.jp>
2024-03-18 19:25:57 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit, :update]
+  before_action :set_item, only: [:edit, :update, :show]
+  before_action :redirect_unless_owner, only: [:edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -19,10 +21,30 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to @item, notice: 'Item was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def redirect_unless_owner
+    unless current_user.id == @item.user_id
+      redirect_to root_path, alert: 'アクセス権限がありません'
+    end
+  end
 
   def item_params
     params.require(:item).permit(:image, :item_name, :explanation, :category_id, :condition_id, :delivery_charge_id, :delivery_place_id, :delivery_day_id, :price).merge(user_id: current_user.id)
