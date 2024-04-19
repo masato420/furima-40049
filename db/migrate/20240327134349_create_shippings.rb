9bdaf5855458b7810eaa1a0bf13ba18a55f43b63
masato420 <yamamoto.3583@outlook.jp>
2024-04-20 00:25:29 +0900

    修正

--- db/migrate/20240327134349_create_shippings.rb
+++ db/migrate/20240327134349_create_shippings.rb
@@ -1,6 +1,6 @@
 class CreateShippings < ActiveRecord::Migration[7.0]
   def change
-    create_tabl   e :shippings do |t|
+    create_tabl    :shippings do |t|
       t.string     :post_code,           null: false
       t.integer    :delivery_place_id,   null: false
       t.string     :city,                null: false
